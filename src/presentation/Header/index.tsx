a2933109cc39fccc8ccd2cd3a6991b5c8d3b7679
Vinicios Neves <vinicios.neves@mac>
2024-11-01 15:25:09 +0000

    video 3.1

--- src/presentation/Header/index.tsx
+++ src/presentation/Header/index.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
-import { IconLogo } from "../Icons"
 import { Container, StyledHeader, List, ListItem } from "./styles"
 import { useAuthContext } from "../../app/hooks/useAuthContext"
 import { AuthenticadedActionList } from "./AuthenticadedActionList"
 import { UnauthenticadedActionList } from "./UnauthenticadedActionList"
+import { IconLogo } from "../../components/Icons"
 
 export const Header = () => {
     const { session } = useAuthContext()
